85300be4e3254f7e73e4ca0f99383cb778685e9a
Caleb D. Sim <103973059+CodeLamp168@users.noreply.github.com>
2023-02-12 09:32:17 +0800

    Replacing File

--- starter-code/components/nav.js
+++
@@ -1,8 +0,0 @@
-//nav btn for responsive layout
-const navArea = document.querySelector('.nav-js')
-const navOpenBtn = document.querySelector('.nav-open-btn')
-const navCloseBtn = document.querySelector('.nav-exit-btn')
-
-navOpenBtn.addEventListener('click', () => navArea.classList.add('mobile-nav-active'))
-navCloseBtn.addEventListener('click', () => navArea.classList.remove('mobile-nav-active'))
-
